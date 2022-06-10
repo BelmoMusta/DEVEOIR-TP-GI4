e7b72f8b6ead298d6e984eb3cb766e47850d051b
Benziza <user.name>
2022-06-10 19:50:47 +0100

    Tous les fonctions

--- src/script/populate.sql
+++ src/script/populate.sql
@@ -1,5 +1,18 @@
 
-INSERT INTO  materiel(name, code) values ( 'Livre' ,'LI'  );
-INSERT INTO  materiel(name, code) values ( 'Chaise' ,'CH'  );
+INSERT INTO  materiel values ( 1,'Odyssey' ,'1','Livre',true,false );
+INSERT INTO  materiel values ( 2,'The noticer' ,'2','Livre',true,false );
+INSERT INTO  materiel values ( 3,'La boite à merveilles' ,'3','Livre',true,false );
+INSERT INTO  materiel values ( 4,'Antigone' ,'4','Livre',true,false );
+INSERT INTO  materiel values ( 5,'Le bourgeois gentilhomme' ,'5','Livre',true,false );
+
+INSERT INTO  materiel values ( 6,'Sofa' ,'6','Chaise',true,false );
+INSERT INTO  materiel values ( 7,'Tabouret' ,'7','Chaise',true,false );
+INSERT INTO  materiel values ( 8,'La chaise électrique' ,'8','Chaise',true,false );
+INSERT INTO  materiel values ( 9,'La chaise longue' ,'9','Chaise',true,false );
+INSERT INTO  materiel values ( 10,'La chaise pliante' ,'10','Chaise',true,false );
+
+INSERT INTO  user values ( 1,'Benziza' ,'000','admin');
+INSERT INTO  user values ( 2,'Chadli','123','user' );
+
 
 
--- src/script/schema.sql
+++ src/script/schema.sql
@@ -1,13 +1,43 @@
 DROP TABLE IF EXISTS materiel;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS usermateriel;
+
 
 CREATE TABLE materiel
 (
-    id   INT AUTO_INCREMENT PRIMARY KEY,
+    idMateriel   INT AUTO_INCREMENT PRIMARY KEY,
 
     name VARCHAR(250) NOT NULL,
 
-    code VARCHAR(250) NOT NULL,
+    code VARCHAR(250)  NOT NULL UNIQUE,
 
+    typeMateriel VARCHAR(250) NOT NULL,
+
+    isDisponible BOOLEAN NOT NULL,
+
+    isAllouer BOOLEAN NOT NULL,
 );
 
+CREATE TABLE user
+(
+    idUser   INT AUTO_INCREMENT PRIMARY KEY,
 
+    userName VARCHAR(250) NOT NULL,
+
+    password VARCHAR(250) NOT NULL,
+
+    role VARCHAR(250) NOT NULL,
+);
+
+CREATE TABLE usermateriel
+(
+    idUserMateriel   INT AUTO_INCREMENT PRIMARY KEY,
+
+    idUser INT NOT NULL,
+
+    code VARCHAR(250) NOT NULL,
+
+    FOREIGN KEY(idUser) REFERENCES user,
+
+    FOREIGN KEY(code) REFERENCES materiel,
+);
