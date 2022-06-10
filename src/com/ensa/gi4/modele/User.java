d9e4b050568eb584a4e098353663ebe82e5278cd
Saad <saadseni@gmail.com>
2022-06-10 19:02:06 +0100

    First commit

---
+++ src/com/ensa/gi4/modele/User.java
@@ -0,0 +1,43 @@
+package com.ensa.gi4.modele;
+
+public class User {
+    private long userId;
+    private String name;
+    private String password;
+    private String role;
+
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+}
